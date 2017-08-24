d78ba2d4882db40b4a046de078e6ae303197e7e7
Jim Robinson <jim@croud.co.uk>
2017-08-24 15:10:40 +0100

    Update route prefix

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 use CroudTech\RecurringTaskScheduler\Http\Controllers\ScheduleController;
 use CroudTech\RecurringTaskScheduler\Http\Controllers\ScheduleEventController;
 
-Route::group(['prefix' => 'api/schedules', 'middleware' => ['api']], function () {
+Route::group(['prefix' => 'api/croudtech', 'middleware' => ['api']], function () {
     Route::resource('schedule', ScheduleController::class);
     Route::resource('schedule.schedule-event', ScheduleEventController::class);
 });
