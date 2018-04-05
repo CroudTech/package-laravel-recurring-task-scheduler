523c455c754066675217df8fbe7ae468d6eacab0
Paul Watkins <paul.watkins@croud.co.uk>
2018-04-05 17:06:19 +0100

    fix conditional in filter

--- src/Repository/ScheduleRepository.php
+++ src/Repository/ScheduleRepository.php
@@ -72,7 +72,7 @@ class ScheduleRepository extends BaseRepository implements RepositoryContract, S
         $current_dates = $schedule->scheduleEvents()->get()->pluck('date');
         $deprecated_dates = collect($current_dates)->diff($parser_dates)->toArray();
         $new_dates = collect($parser_dates)->diff($current_dates)->filter(function($date) use ($today, $from_today) {
-            return ($from_today && $date >= $today) || true;
+            return !$from_today || $date >= $today;
         });
 
         $schedule->scheduleEvents()->whereIn('date', $deprecated_dates)->get()->each(function($scheduleEvent) {
