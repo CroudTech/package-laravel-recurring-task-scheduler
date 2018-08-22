1d2a876d91bfbb10fda81a01cbc42fd24e80e796
Connor Sidwell <azrellion@hotmail.com>
2018-08-22 14:47:02 +0100

    Adding test for paused events

--- tests/unit_tests/ConsoleTests/ScheduleRunTest.php
+++ tests/unit_tests/ConsoleTests/ScheduleRunTest.php
@@ -2,11 +2,13 @@
 namespace CroudTech\RecurringTaskScheduler\Tests\ConsoleTests;
 
 use Carbon\Carbon;
-use CroudTech\RecurringTaskScheduler\Library\ScheduleParser\Periodic as PeriodicParser;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Artisan;
 use CroudTech\RecurringTaskScheduler\Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Event;
+use CroudTech\RecurringTaskScheduler\Model\ScheduleEvent;
+use CroudTech\RecurringTaskScheduler\Library\ScheduleParser\Periodic as PeriodicParser;
 
 class ScheduleEventTest extends TestCase
 {
@@ -51,12 +53,41 @@ class ScheduleEventTest extends TestCase
         $valid_schedule_date = Carbon::parse('2017-01-01 00:00:00');          // create testing date
         Carbon::setTestNow($valid_schedule_date);
 
-
         Artisan::call('croudtech:schedule:execute');
         $event_query = $schedule->scheduleEvents()->whereBetween('date', [$valid_schedule_date->copy()->startOfDay(), $valid_schedule_date->copy()->endOfDay()]);
+
         $this->assertEquals(1, $event_query->count());
         $event_query->each(function ($schedule_event) {
             $this->assertTrue($schedule_event->trigger_success);
+        });
+    }
+
+    public function testScheduleEventTriggerEventPaused()
+    {
+        Carbon::setTestNow(Carbon::parse('2017-01-01 00:00:00'));
+        $this->migrate();
+        $scheduleable = new \CroudTech\RecurringTaskScheduler\Tests\App\Model\TestScheduleable(['test_success' => true, 'name' => __CLASS__ . '::' . __METHOD__]);
+        $scheduleable->save();
+        $schedule = new \CroudTech\RecurringTaskScheduler\Model\Schedule();
+        $schedule->range_start = '2017-01-01 00:00:00';
+        $schedule->range_end = '2017-01-05 00:00:00';
+        $schedule->period = 'days';
+        $schedule->scheduleable()->associate($scheduleable);
+        $schedule->save();
+
+        $valid_schedule_date = Carbon::parse('2017-01-01 00:00:00');          // create testing date
+        Carbon::setTestNow($valid_schedule_date);
+
+        $scheduleEvent = ScheduleEvent::where('schedule_id', $schedule->id)->where('date', $valid_schedule_date)->first();
+        $scheduleEvent->paused = true;
+        $scheduleEvent->save();
+        
+        Artisan::call('croudtech:schedule:execute');
+        $event_query = $schedule->scheduleEvents()->whereBetween('date', [$valid_schedule_date->copy()->startOfDay(), $valid_schedule_date->copy()->endOfDay()]);
+        
+        $this->assertEquals(1, $event_query->count());
+        $event_query->each(function ($schedule_event) {
+            $this->assertEquals(null, $schedule_event->trigger_success);
         });
     }
 }
