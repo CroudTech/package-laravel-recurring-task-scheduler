c587ad1f0f8aeeceb3bfa5e5c33fa741279b5b94
Connor Sidwell <azrellion@hotmail.com>
2018-09-19 16:59:04 +0100

    Adding times to repository to store in DB

--- src/Repository/ScheduleRepository.php
+++ src/Repository/ScheduleRepository.php
@@ -28,8 +28,9 @@ class ScheduleRepository extends BaseRepository implements RepositoryContract, S
         $schedule_attributes = $this->getTransformer()->transformDefinitionToScheduleAttributes($parser->getDefinition());
         $schedule_attributes = $this->preCreate($this->parseData($schedule_attributes));
         $schedule = $this->make($schedule_attributes);
-        isset($scheduleable) ? $schedule->scheduleable()->associate($scheduleable) : null;
 
+        isset($scheduleable) ? $schedule->scheduleable()->associate($scheduleable) : null;
+        $schedule->times = isset($definition['times']) ? json_encode($definition['times']) : null;
         $schedule->entity_id = isset($definition['entity_id']) ? $definition['entity_id'] : null;
         $schedule->entity_callback_url = isset($definition['entity_callback_url']) ? $definition['entity_callback_url'] : null;
         $schedule->entity_callback_params = isset($definition['entity_callback_params']) ? $definition['entity_callback_params'] : null;
