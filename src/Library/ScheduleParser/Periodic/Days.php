c8df4fa7cb58eb8eac89c431bfb7ccb21c7c87f0
Azrellion <azrellion@hotmail.com>
2018-09-20 15:18:53 +0100

    Feature/multiple schedules daily (#19)
    * Adding new field that is json of times.
    
    * Adding times to repository to store in DB
    
    * Added tests for multiple times in day. Also added check to avoid array error
    
    * Fixing dates to new format
    
    * Added new multi definitions for America and Australia

--- src/Library/ScheduleParser/Periodic/Days.php
+++ src/Library/ScheduleParser/Periodic/Days.php
@@ -20,8 +20,12 @@ class Days extends Base implements ScheduleParserContract
 
             // Prevent iteration over more than 3650 days to stop incorrect definition from causing infinite loops
             while ($current_date->lte($this->getRangeEnd()) && count($this->generated) < 3650) {
+                $times = $this->getTimes() ? $this->validateTimes($this->getTimes()) : [$current_date->toTimeString()];
                 if ($current_date->lte($this->getRangeEnd()) && $current_date->gte($this->getRangeStart())) {
-                    $this->generated[] = $current_date->copy();
+                    foreach ($times as $time) {
+                        $current_date->setTime(...explode(':', $time));
+                        $this->generated[] = $current_date->copy();
+                    }
                 }
 
                 $modify_method = sprintf('add%s', ucfirst(camel_case($this->definition['period'])));
