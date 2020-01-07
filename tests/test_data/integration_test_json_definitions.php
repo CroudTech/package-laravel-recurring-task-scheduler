c27e031bc0c43c16519cfca63ed9f914a17a529e
Paul Watkins <paul.watkins@croud.co.uk>
2020-01-07 19:36:15 +0000

    add test for VE-1733

--- tests/test_data/integration_test_json_definitions.php
+++ tests/test_data/integration_test_json_definitions.php
@@ -336,6 +336,17 @@ return collect([
             '2020-11-01T09:00:00+00:00',
         ],
     ],
+    'Every two months starting from december' => [
+        '{ "timezone": "Europe/London", "range": { "start": "2019-11-30", "end": "2020-11-30" }, "time_of_day": "09:00", "type": "periodic", "interval": 2, "period": "months", "day_number": 1, "week_number": false, "days": { }, "months": { } }',
+                [
+            '2019-12-01T09:00:00+00:00', 
+            '2020-02-01T09:00:00+00:00',
+            '2020-04-01T08:00:00+00:00',
+            '2020-06-01T08:00:00+00:00',
+            '2020-08-01T08:00:00+00:00',
+            '2020-10-01T08:00:00+00:00',
+        ],
+    ]
 ])->map(function ($row) {
     foreach ($row[1] as $k => $expected_date) {
         $row[1][$k] = \Carbon\Carbon::parse($expected_date)->setTimezone('UTC')->format('c');
