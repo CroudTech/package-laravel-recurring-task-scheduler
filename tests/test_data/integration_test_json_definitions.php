7352e540743bcf6523bcd43369c4cf2783bb4504
Paul Watkins <paul.watkins@croud.co.uk>
2020-01-07 14:39:35 +0000

    Add test to check overlapping month/year

--- tests/test_data/integration_test_json_definitions.php
+++ tests/test_data/integration_test_json_definitions.php
@@ -326,6 +326,16 @@ return collect([
             '2017-11-13T09:00:00+03:00', // Mon 13 Nov 2017 00:00:00 +01:00
         ],
     ],
+    'Same day every three months spanning a new year' => [
+        '{ "timezone": "Europe/London", "range": { "start": "2019-11-01", "end": "2020-11-30" }, "time_of_day": "09:00", "type": "periodic", "interval": 3, "period": "months", "day_number": 1, "week_number": false, "days": { }, "months": { } }',
+        [
+            '2019-11-01T09:00:00+00:00', 
+            '2020-02-01T09:00:00+00:00',
+            '2020-05-01T08:00:00+00:00',
+            '2020-08-01T08:00:00+00:00',
+            '2020-11-01T09:00:00+00:00',
+        ],
+    ],
 ])->map(function ($row) {
     foreach ($row[1] as $k => $expected_date) {
         $row[1][$k] = \Carbon\Carbon::parse($expected_date)->setTimezone('UTC')->format('c');
